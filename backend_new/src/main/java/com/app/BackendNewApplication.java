a61a30f6236048caea909563bd24b32ead43cb0a
Edwin guevara <egbmaster2007@gmail.com>
2026-03-06 21:40:50 -0500

    fix: Exclude more JDBC/JPA auto-configurations

--- backend_new/src/main/java/com/app/BackendNewApplication.java
+++ backend_new/src/main/java/com/app/BackendNewApplication.java
@@ -3,9 +3,15 @@ package com.app;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.data.r2dbc.repository.config.EnableR2dbcRepositories;
 
-@SpringBootApplication(exclude = { DataSourceAutoConfiguration.class })
+@SpringBootApplication(exclude = {
+        DataSourceAutoConfiguration.class,
+        DataSourceTransactionManagerAutoConfiguration.class,
+        HibernateJpaAutoConfiguration.class
+})
 public class BackendNewApplication {
     public static void main(String[] args) {
         SpringApplication.run(BackendNewApplication.class, args);
