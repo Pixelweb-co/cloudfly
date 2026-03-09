9f9900997694c1c162a8e151419042575521e872
Edwin guevara <egbmaster2007@gmail.com>
2026-03-09 02:15:29 -0500

    fix(dto): force isAvailable field name in JSON serialization

--- backend_new/src/main/java/com/app/dto/AvailabilityResponse.java
+++ backend_new/src/main/java/com/app/dto/AvailabilityResponse.java
@@ -1,5 +1,6 @@
 package com.app.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -10,6 +11,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class AvailabilityResponse {
+    @JsonProperty("isAvailable")
     private boolean isAvailable;
     private String message;
 }
