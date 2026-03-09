23ae88b563744e973282d62a8f31926cbb03a700
Edwin guevara <egbmaster2007@gmail.com>
2026-03-09 14:59:39 -0500

    Update AuthResponse and AuthController to include user object for frontend session

--- backend_new/src/main/java/com/app/dto/AuthResponse.java
+++ backend_new/src/main/java/com/app/dto/AuthResponse.java
@@ -1,5 +1,6 @@
 package com.app.dto;
 
+import com.app.persistence.entity.UserEntity;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -14,4 +15,5 @@ public class AuthResponse {
     private String message;
     private String jwt;
     private boolean status;
+    private UserEntity user;
 }
