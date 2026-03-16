9ac4dbd4bed773131ce85b2da05105365b38688f
Edwin guevara <egbmaster2007@gmail.com>
2026-03-16 09:10:12 -0500

    fix: subscription module persistence and reorder account setup flow

--- backend_new/src/main/java/com/app/persistence/entity/SubscriptionModuleEntity.java
+++ backend_new/src/main/java/com/app/persistence/entity/SubscriptionModuleEntity.java
@@ -4,7 +4,8 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.Transient;
+import org.springframework.data.domain.Persistable;
 import org.springframework.data.relational.core.mapping.Column;
 import org.springframework.data.relational.core.mapping.Table;
 
@@ -13,12 +14,27 @@ import org.springframework.data.relational.core.mapping.Table;
 @NoArgsConstructor
 @AllArgsConstructor
 @Table("subscription_modules")
-public class SubscriptionModuleEntity {
+public class SubscriptionModuleEntity implements Persistable<Object> {
 
-    @Id
     @Column("subscription_id")
     private Long subscriptionId;
 
     @Column("module_id")
     private Long moduleId;
+
+    @Transient
+    @Builder.Default
+    private boolean isNew = true;
+
+    @Override
+    public Object getId() {
+        // Return null or a composite key representation if needed, 
+        // but for pure inserts, R2DBC just needs isNew() to be true.
+        return null;
+    }
+
+    @Override
+    public boolean isNew() {
+        return isNew;
+    }
 }
