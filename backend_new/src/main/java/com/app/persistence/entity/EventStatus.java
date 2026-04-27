f3fcd9065936b2db8c6bd85d7c38ea3c17d95586
Edwin guevara <egbmaster2007@gmail.com>
2026-04-26 19:31:21 -0500

    refactor: Final removal of scheduler from backend_new

--- backend_new/src/main/java/com/app/persistence/entity/EventStatus.java
+++
@@ -1,8 +0,0 @@
-package com.app.persistence.entity;
-
-public enum EventStatus {
-    SCHEDULED,
-    EXECUTED,
-    FAILED,
-    CANCELED
-}
--- backend_new/src/main/java/com/app/persistence/entity/JobStatus.java
+++
@@ -1,8 +0,0 @@
-package com.app.persistence.entity;
-
-public enum JobStatus {
-    PENDING,
-    RUNNING,
-    DONE,
-    FAILED
-}
