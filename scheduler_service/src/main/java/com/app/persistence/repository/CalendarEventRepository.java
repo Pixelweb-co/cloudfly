57fd2b24c3090d9e4be2ff324cf1827462c457bb
Edwin guevara <egbmaster2007@gmail.com>
2026-04-28 13:54:09 -0500

    Fix: Add missing Mono import

--- scheduler_service/src/main/java/com/app/persistence/repository/CalendarEventRepository.java
+++ scheduler_service/src/main/java/com/app/persistence/repository/CalendarEventRepository.java
@@ -4,6 +4,7 @@ import com.app.persistence.entity.CalendarEventEntity;
 import org.springframework.data.r2dbc.repository.Query;
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 import java.time.LocalDateTime;
 
