693329711568ef21b98f662a25685105934d987d
Edwin guevara <egbmaster2007@gmail.com>
2026-05-12 10:38:28 -0500

    tools agent

--- scratch/list_databases.js
+++ scratch/list_databases.js
@@ -2,16 +2,10 @@ const { Client } = require('ssh2');
 const fs = require('fs');
 
 const conn = new Client();
-const config = {
-  host: '109.205.182.94',
-  port: 22,
-  username: 'root',
-  privateKey: fs.readFileSync('C:/Users/Edwin/.ssh/id_rsa_cloudfly'),
-  readyTimeout: 60000
-};
+const config = { host: '109.205.182.94', port: 22, username: 'root', privateKey: fs.readFileSync('C:/Users/Edwin/.ssh/id_rsa_cloudfly'), readyTimeout: 60000 };
 
 conn.on('ready', () => {
-  const cmd = 'docker ps';
+  const cmd = 'docker exec mysql mysql -u root -pwidowmaker -e "SHOW DATABASES;"';
   conn.exec(cmd, (err, stream) => {
     if (err) throw err;
     stream.on('close', () => conn.end()).on('data', d => process.stdout.write(d)).stderr.on('data', d => process.stderr.write(d));
